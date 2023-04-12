dfa23cfb472f863002bc320250a6a34aec84f543
John Guerrero <jpguerrero1022@gmail.com>
2023-04-12 18:17:31 -0500

    created new branch for css formatting and javascript

--- assets/script.js
+++ assets/script.js
@@ -1,120 +1,157 @@
-var quizArea = document.getElementById("quiz")
-var start = document.getElementById("startQuiz")
+var quizArea = document.getElementById("quiz");
+var startBtn = document.getElementById("startBtn");
+var countdown = document.getElementById("countDown");
 
-var timeCount = 60
+var score;
 
-var questions = [ 
-    {
-        question:"1st question",
-        answerchoice: ["1st answer choice","2nd", "3rd","4","5"],
-        correctAnswer: "correct answer",
-
-        question:"1st question",
-        answerchoice: ["1st answer choice","2nd", "3rd","4","5"],
-        correctAnswer: "correct answer",
-
-        question:"1st question",
-        answerchoice: ["1st answer choice","2nd", "3rd","4","5"],
-        correctAnswer: "correct answer",
-
-        question:"1st question",
-        answerchoice: ["1st answer choice","2nd", "3rd","4","5"],
-        correctAnswer: "correct answer",
-
-        question:"1st question",
-        answerchoice: ["1st answer choice","2nd", "3rd","4","5"],
-        correctAnswer: "correct answer",
-
-        question:"1st question",
-        answerchoice: ["1st answer choice","2nd", "3rd","4","5"],
-        correctAnswer: "correct answer",
-    
-    }
+var questions = [
+  {
+    question: "1st question",
+    answerChoices: ["1st answer choice", "2nd", "3rd", "4th"],
+    correctAnswer: "2nd"
+  },
+  {
+    question: "2nd question",
+    answerChoices: ["1st answer choice", "2nd", "3rd", "4th"],
+    correctAnswer: "2nd"
+  },
+  {
+    question: "3rd question",
+    answerChoices: ["1st answer choice", "2nd", "3rd", "4th"],
+    correctAnswer: "2nd"
+  },
+  {
+    question: "4th question",
+    answerChoices: ["1st answer choice", "2nd", "3rd", "4th"],
+    correctAnswer: "2nd"
+  },
+  {
+    question: "5th question",
+    answerChoices: ["1st answer choice", "2nd", "3rd", "4th", "5"],
+    correctAnswer: "2nd"
+  }
 ]
 
 var currentQuestion = 0;
 
-function startgame(event) {
-    event.preventDefault();
-    // start the quiz
-    // start timer
-    //1. create a question, diff function
-    generateQuestion()
-    //2. create answers
-for (var i=0; i<5; i++) {
+function startGame(event) {
+  event.preventDefault();
+  // start the quiz!
+  // 1. Start timer
+  // 2. create a question
+  // 3. create answer choices
+  // 4. add event listeners to my answer choice buttons and that will validate whether they chose the right answer or not
+  // 5. move on to next question
 
-}
-    //3. add answer event listeners
-    // will validate if answer choice is right
-    //4. move on to next question
-}
-
-function generateQuestion() {
-    var questions = questions[currentQuestion].question;
-    //create an element(p,div)
-    //write into element using textContent
-    //append the question to the element in our quiz are (appendchild)
-
-    //add event listener to move to next question
-    generateAnswerChoice()
-}
-
-function generateAnswerChoice() {
-    //for loop for all answer choices questions[currentQuestion].answerChoices.length
-    //create an element(button)
-    //write into element using out anser choice variable textContent
-    //add event listener to all btn.addEventListener('click", validateAnswer)
-    //append the question to the element in our quiz are (appendchild)
-}
-
-function validateAnswer(event) {
-    event.preventDefault();
-    //grab text of button clicked (event.target.textContent)
-    //add condition statement to test userChoice === correctAnswer
-    // if true, run correct answer code, add css change or interaction to show its correct
-    //if false, add the opposite (decrease timer by 10)
-    //check if youve reached end of questions array, condition, if(currentQuestion === questions.length)
-    //end the game (call endgame)
-
-    //move onto next question
-    // currentQuestion++
-
-    //reset quiz area (quizArea.innerhtml = '', loop using .removeChild()?)
-    generateQuestion();
+  // this function will start your timer
+  startTimer()
+  // this function will kick off rendering the question and answers to the page
+  generateQuestion();
 }
 
 function startTimer() {
-    timer = setInterval(function() {
-        timerDisplay.textContent = timeCounttimeCount--;
-    },1000)
-    
+    var timeLeft = 5;
+
+    var timeInterval = setInterval(function() {
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      countdown.textContent = timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        countdown.textContent = timeLeft;
+        timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      countdown.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval)
+      endGame()
+      // Call the `displayMessage()` function
+      // displayMessage();
+    }
+  }, 1000)
 }
 
-function endGame() {
-    //end game whether it reaches the end of the quiz or if time runs out
-    //display none quiz area and display end game div
-    //score
-    var score=timeCount;
-    //display score
+function generateQuestion() {
 
+  // create an element (p, div)
+    
+  // write into that element using our question variable (textContent)
+  
+  // append that question element into our quiz area (appendChild)
+
+  // generateAnswerChoices
+  generateAnswerChoices();
+}
+
+function generateAnswerChoices() {
+  // for loop i < questions[currentQuestion].answerChoices.length
+  // create an element (button)
+
+  // write into that element using our answerChoices variable (textContent)
+
+  // add event listener btn.addEventListener("click", validateAnswer)
+  
+  // append that question element into our quiz area (appendChild)
+
+}
+
+function validateAnswer(event) {
+  event.preventDefault();
+  // grab text of button that was clicked (event.target.textContent)
+
+  // conditional statement test userChoice === correctAnswer
+  // true
+  //    correct answer code
+  // false
+  //    incorrect answer
+  //    decrease timer by 10 secs
+  
+  // move onto the next question
+
+  // currentQuestion++
+  // conditional statement to check if you've reached the end of the questions array if(currentQuestion === questions.length)
+  // end the game (call endGame())
+    endGame()
+  // reset quiz area (quizArea.innerHTML = "", loop using .removeChild())
+
+  // call generateQuestion again to start on the next question
+  generateQuestion();
+}
+
+
+
+function endGame() {
+  // end game whether it reaches the end of the quiz or time runs out
+  // display none quiz area and display end game div
+
+  // score
+  score = countdown;
+  // display score
+
+  // display high score
 }
 
 function saveScore(event) {
-    event.preventDefault();
-    var scoreObj = {
-        initials:event.target.child,
-        score:timeCount,
-    }
-    //sets the score into local storage
-    localStorage.setItem('score',JSON.stringify(scoreObj));
-}
+  event.preventDefault();
 
+  var scoreObj = {
+    intials: event.target.children[0].value,
+    score: countdown
+  }
+  // sets the score into local storage
+  localStorage.setItem("score", JSON.stringify(scoreObj));
+}
 
 function getScore() {
-    //get high schore out of local
+  // get high score out of localstorage
+  var score = JSON.parse(localStorage.getItem("score"))
+  // display to end game div
+  
 }
 
+startBtn.addEventListener("click", startGame);
 
-
-startQuiz.addEventListener('click', startgame);
-scoreForm.addEventListener('submit', saveScore);
+//score.addEventListener("submit", saveScore)
